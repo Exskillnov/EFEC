e970369f4162a32bede79a6834a245e8d46403b0
Leandro <leandrolopezhegui@gmail.com>
2021-03-06 23:10:04 -0300

    Obtencion y extracción de archivos

--- README.md
+++ README.md
@@ -4,6 +4,10 @@
 
 Hace prácicamente poco tiempo perdí archivos en excel y pdf de mi computadora y estoy necesitando recuperarlos, gran parte de ellos fueron enviados y logré recuperar versiones anteriores con los nombres anteriores de esos archivos, el objetivo principal de este programa es comparar los nombres de los archivos que tengo en mi disco y encontrar la última versión (partiendo de que la última versión es el último archivo enviado a partir de la fecha de envio) enviada a un destinatario para así recuperarlo.
 
+## Antes de Comenzar
+
+Es necesario habilitar IMAP en las configuraciones de Gmail y al mismo tiempo habilitar el acceso al correo por medio de aplicaciones no seguras en el caso de que por algún motivo Google intente bloquear el acceso al programa detectándolo como dispositivo.
+
 ## Seguimiento
 
 Para recuperar los archivos hasta ahora se manejará Python utilizando las librerias echo  `imaplib`, `email` y otros...
--- filesOnGmail.py
+++ filesOnGmail.py
@@ -1,3 +1,30 @@
 import imaplib, email
 
-print ("Hello")
+username = input('Ingrese Usuario: ')
+password = input('Ingrese Contraseña: ')
+
+mail = imaplib.IMAP4_SSL('imap.gmail.com')
+mail.login(username, password)
+
+folder = mail.select("[Gmail]/Enviados") #Ingresa a la carpeta de archivos enviados
+print(folder)
+
+result, data = mail.uid('search', None, 'ALL') #Recibe todos los mensajes
+
+listOfItems = data[0].split() #Crea una lista con todas las IDs codificadas
+
+print(listOfItems)
+
+for item in listOfItems:
+    result2, emailData = mail.uid('fetch', item, '(RFC822)')
+    rawEmail = emailData[0][1].decode('utf-8')
+    emailMessage = email.message_from_string(rawEmail)
+    if emailMessage.get_content_maintype() == 'multipart': #mensaje con multipartes
+        for part in emailMessage.walk():
+            if part.get('Content-Disposition') is None: continue
+            print (emailMessage["Date"]) #Recupera la fecha del mensaje enviado
+            filename = part.get_filename()
+            fp = open(filename, 'wb')
+            fp.write(part.get_payload(decode=True))
+            fp.close()
+            print ('{0} Guardado!'.format(filename))
